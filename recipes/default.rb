792cb4c3b19b4d4b7b73bff4e73a5112480deae3
Tony Beckham <tony.beckham@gmail.com>
2015-03-10 12:27:20 -0700

    Install openssl-devel dep before including cassandra cookbook.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,10 +1,11 @@
 include_recipe 'java'
+
+execute 'install openssl-devel' do
+  command 'yum -y install openssl-devel'
+end
+
 include_recipe 'cassandra'
 include_recipe 'midokura::zookeeper'
 include_recipe 'midokura::midolman'
 include_recipe 'midokura::midonet-api'
 #include_recipe 'midokura::midonet-cp'
-
-execute 'install openssl-devel' do
-  command 'yum -y install openssl-devel'
-end
