86ad64adfde3ff78e345dbc8df2a943a7ea241c2
Tony Beckham <tony.beckham@gmail.com>
2015-03-11 13:19:00 -0700

    forgot to save changes to default to install stuff

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,13 @@
 include_recipe 'java'
 
-# package 'openssl-devel'
-# include_recipe 'cassandra'
+yum_repository "datastax" do
+  description "DataStax Repo for Apache Cassandra"
+  url "http://rpm.datastax.com/community"
+  gpgcheck = false
+  action :create
+end
+
+package ['dsc20-2.0.12-1', 'cassandra20-2.0.12-1']
 
 include_recipe 'midokura::zookeeper'
 include_recipe 'midokura::midolman'
