1ea1f551b3d269ce1962e335e95a8dd5ac1804eb
Tony Beckham <tony.beckham@gmail.com>
2015-03-11 13:43:16 -0700

    Break up package install into 2 lines.

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,11 +3,12 @@ include_recipe 'java'
 yum_repository "datastax" do
   description "DataStax Repo for Apache Cassandra"
   url "http://rpm.datastax.com/community"
-  gpgcheck = false
+  gpgcheck = False
   action :create
 end
 
-package ['dsc20-2.0.12-1', 'cassandra20-2.0.12-1']
+package 'dsc20-2.0.12-1'
+package 'cassandra20-2.0.12-1'
 
 include_recipe 'midokura::zookeeper'
 include_recipe 'midokura::midolman'
