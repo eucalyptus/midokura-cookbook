d3842c324ee482a7f200a371352de1f97732c3f3
Tony Beckham <tony.beckham@gmail.com>
2015-03-12 10:25:06 -0700

    Set FQDN for cassandra listen and RPC then start datastax and cassandra

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,6 +12,20 @@ end
 package 'dsc20'
 package 'cassandra20'
 
+execute "Set cassandra listening address" do
+ command "sed -i -e 's/localhost/#{node['fqdn']}/g' /etc/cassandra/conf/cassandra.yaml"
+end
+
+service "dsc20" do
+  action [ :enable, :start ]
+  supports :status => true, :start => true, :stop => true, :restart => true
+end
+
+service "cassandra20" do
+  action [ :enable, :start ]
+  supports :status => true, :start => true, :stop => true, :restart => true
+end
+
 include_recipe 'midokura::zookeeper'
 include_recipe 'midokura::midolman'
 include_recipe 'midokura::midonet-api'
