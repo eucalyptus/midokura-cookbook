b733d2b5fd1b4aaa7b608f521de9b7f8eb3d2cd3
Tony Beckham <tony.beckham@gmail.com>
2015-03-12 09:33:03 -0700

    Set metdata_expire to 1 second so yum updates quickly

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,7 @@ yum_repository "datastax" do
   description "DataStax Repo for Apache Cassandra"
   url "http://rpm.datastax.com/community"
   gpgcheck false
+  metadata_expire "1"
   sslverify false
   action :create
 end
