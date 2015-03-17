9449cbb99d79529b5ed7648c358281c6b246a093
Vic Iglesias <viglesiasce@gmail.com>
2015-03-17 00:19:37 -0700

    Change mkdir to chef directory resource

--- recipes/zookeeper.rb
+++ recipes/zookeeper.rb
@@ -4,9 +4,7 @@ if (node['midokura']['zookeepers'] == [])
   raise "Please set the Midokura zookeepers list attribute in your environment: midokura->zookepers"
 end
 
-execute "ZOOKEEPER: sym link step 1 create dir" do
- command "mkdir /usr/java"
-end
+directory "/usr/java"
 
 execute "ZOOKEEPER: sym link step 2 create link" do
  command "ln -s /usr/lib/jvm/java-1.7.0-openjdk-1.7.0.75.x86_64/jre/ /usr/java/default"
