638499939e1c8f549cb03697b20bb5f933b68087
Tony Beckham <tony.beckham@gmail.com>
2015-03-17 07:16:41 -0700

    Cheffy way to create dir and symlink

--- recipes/zookeeper.rb
+++ recipes/zookeeper.rb
@@ -4,12 +4,11 @@ if (node['midokura']['zookeepers'] == [])
   raise "Please set the Midokura zookeepers list attribute in your environment: midokura->zookepers"
 end
 
-execute "ZOOKEEPER: sym link step 1 create dir" do
- command "mkdir /usr/java"
-end
+directory "/usr/java"
 
-execute "ZOOKEEPER: sym link step 2 create link" do
- command "ln -s /usr/lib/jvm/java-1.7.0-openjdk-1.7.0.75.x86_64/jre/ /usr/java/default"
+link "/usr/java/default" do
+  to "/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.75.x86_64/jre/"
+  only_if 'test -f /usr/lib/jvm/java-1.7.0-openjdk-1.7.0.75.x86_64/jre/'
 end
 
 package "zookeeper" do
