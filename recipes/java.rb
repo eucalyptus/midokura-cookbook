b9ad793c58234d8143fabff109b7940f19fe5920
Tony Beckham <tony.beckham@gmail.com>
2015-03-16 19:04:51 -0700

    Do not try to set JAVA_HOME

--- recipes/java.rb
+++ recipes/java.rb
@@ -1,9 +1,1 @@
 package node['java']['pacakge']
-
-execute "JAVA: set JAVA_HOME" do
- command "export JAVA_HOME=#{node['java']['jdk_location']}"
-end
-
-execute "JAVA: set JAVA_HOME" do
- command "echo 'export $JAVA_HOME' >> /root/.bashrc"
-end
